f0bd253392a35a23190a13f86893a7545aff2dcf
Favio Gomez <favio.gz02@gmail.com>
2024-09-17 12:56:28 -0300

    Agregado test mock para traer eventos en una lista

--- src/test/java/com/tallerwebi/infraestructura/RepositorioEventoImplTest.java
+++ src/test/java/com/tallerwebi/infraestructura/RepositorioEventoImplTest.java
@@ -5,9 +5,15 @@ import com.tallerwebi.dominio.RepositorioEvento;
 import com.tallerwebi.infraestructura.config.HibernateInfraestructuraTestConfig;
 import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {HibernateInfraestructuraTestConfig.class})
@@ -20,5 +26,6 @@ public class RepositorioEventoImplTest {
     public void init() {
         this.repositorioEvento = new RepositorioEventoImpl(sessionFactory);
     }
+
 
 }
