66cde5fca2707b76ada9498970b9362e0bb80d1e
FavioRei <favio.gz02@gmail.com>
2024-11-07 08:50:58 -0300

    se agregaron test del controlador usuario y servicio usuario

--- src/main/java/com/tallerwebi/presentacion/dto/UsuarioVistaDTO.java
+++ src/main/java/com/tallerwebi/presentacion/dto/UsuarioVistaDTO.java
@@ -3,6 +3,7 @@ package com.tallerwebi.presentacion.dto;
 import com.tallerwebi.dominio.Usuario;
 
 import javax.persistence.Column;
+import java.util.Objects;
 
 public class UsuarioVistaDTO {
 
@@ -11,6 +12,19 @@ public class UsuarioVistaDTO {
     private String telefono;
     private String dni;
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsuarioVistaDTO that = (UsuarioVistaDTO) o;
+        return Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
 
     public UsuarioVistaDTO(Usuario usuario) {
         this.nombre = usuario.getNombre();
---
+++ src/test/java/com/tallerwebi/presentacion/ControladoUsuarioTest.java
@@ -0,0 +1,82 @@
+package com.tallerwebi.presentacion;
+
+import com.tallerwebi.dominio.ServicioLogin;
+import com.tallerwebi.dominio.ServicioUsuario;
+import com.tallerwebi.dominio.Usuario;
+import com.tallerwebi.presentacion.ControladorLogin;
+import com.tallerwebi.presentacion.dto.DatosLoginDTO;
+import com.tallerwebi.presentacion.dto.UsuarioVistaDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.reactive.server.JsonPathAssertions;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
+
+public class ControladoUsuarioTest {
+
+    private ControladorUsuario controladorUsuario;
+    private Usuario usuarioMock;
+    private UsuarioVistaDTO usuarioVistaDTO;
+    private ServicioUsuario servicioUsuarioMock;
+    private HttpSession session;
+    private HttpServletRequest request;
+
+    @BeforeEach
+    public void init() {
+        usuarioMock = mock(Usuario.class);
+        usuarioVistaDTO = new UsuarioVistaDTO(usuarioMock);
+        servicioUsuarioMock = mock(ServicioUsuario.class);
+        controladorUsuario = new ControladorUsuario(servicioUsuarioMock);
+        session = Mockito.mock(HttpSession.class);
+        request = Mockito.mock(HttpServletRequest.class);
+        when(request.getSession()).thenReturn(session);
+    }
+    @Test
+    public void dadoUnUsuarioDTOMockEsteTieneQueEstarCargadoCorrectamenteEnLaVistaUsuario() {
+
+        String email = "a@gmail.com";
+        Usuario usuario = new Usuario(email, "123", "Jose", "Lopez", "123456789", "12345678");
+        UsuarioVistaDTO usuarioVistaDTO = new UsuarioVistaDTO(usuario);
+
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("email")).thenReturn(email);
+        when(servicioUsuarioMock.obtenerUsuarioVistaDTODelRepo(email)).thenReturn(usuario);
+
+        ModelAndView modelAndView = controladorUsuario.mostrarVistaUsuario(request);
+
+
+        assertThat(modelAndView.getViewName(), equalTo("usuario"));
+        ModelMap modelo = modelAndView.getModelMap();
+        assertThat(modelo.containsKey("usuarioVistaDTO"), is(Boolean.TRUE));
+        assertThat(modelo.get("usuarioVistaDTO"), equalTo(usuarioVistaDTO));
+    }
+    @Test
+    public void sinHaberUnUsuarioDTOMockCargadoElModelAndViewDeLaVistaUsuarioDebeEstarVacio() {
+
+        String email = "a@gmail.com";
+
+        when(request.getSession()).thenReturn(session);
+
+        ModelAndView modelAndView = controladorUsuario.mostrarVistaUsuario(request);
+
+
+        assertThat(modelAndView.getViewName(), equalTo("usuario"));
+        ModelMap modelo = modelAndView.getModelMap();
+        assertThat(modelo.containsKey("usuarioVistaDTO"), is(Boolean.FALSE));
+    }
+}
