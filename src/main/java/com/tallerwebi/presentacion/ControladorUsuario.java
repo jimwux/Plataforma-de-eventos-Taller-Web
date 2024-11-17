96df27cdaea594199f097aa3dd440c6727be702a
HidalgoBrian <leonelhidalgo2206@gmail.com>
2024-11-17 20:21:42 -0300

    Boton de cerrar sesion hecho

--- src/main/java/com/tallerwebi/presentacion/ControladorUsuario.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorUsuario.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ControladorUsuario {
@@ -32,5 +35,14 @@ public class ControladorUsuario {
         }
 
         return new ModelAndView("usuario", modelo);
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.GET)
+    public ModelAndView cerrarSesion(HttpServletRequest request){
+            HttpSession session = request.getSession(false);
+            if(session != null){
+                session.invalidate();
+        }
+            return new ModelAndView("redirect:/login");
     }
 }
